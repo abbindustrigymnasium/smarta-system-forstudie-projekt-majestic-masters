e45daff2907a54387e51bf50829d11f8c2499f17
abbsimoga <43879969+abbsimoga@users.noreply.github.com>
2020-12-14 12:37:24 +0100

    moved api to mutations

--- OldMedicine/src/pages/Home.vue
+++ OldMedicine/src/pages/Home.vue
@@ -13,43 +13,13 @@
 <script>
 
 import People from '../components/Home/People.vue'
-import axios from 'axios'
 
 export default {
   name: 'Home',
   mounted () {
     this.$store.commit('user/changePersonPointer', null)
     this.$store.commit('user/changeMedicinePointer', null)
-    this.request('/medicine/patch', { medicine_id: '120912132', amount: 666, interval: 111 }, 'patch')
-  },
-  methods: {
-    request (endpoint, body, method) { // Exempel: ('connections/get', {'client_id': CLIENT_ID_HERE}, 'get'}
-      if (method === 'get') {
-        var queryParameters = '?'
-        for (const key in body) {
-          queryParameters += key + '=' + body[key] + '&'
-        }
-        queryParameters = queryParameters.slice(0, -1)
-
-        axios.get(this.baseURL + endpoint + queryParameters).then(response => {
-          return response.data.body
-        }).catch(function (error) {
-          console.log(error)
-        })
-      } else {
-        axios({
-          method: method,
-          url: this.baseURL + endpoint,
-          headers: {},
-          data: body
-        }).then(response => {
-          console.log(response)
-          return response.data.body
-        }).catch(function (error) {
-          console.log(error)
-        })
-      }
-    }
+    this.$store.commit('user/getInit', 'loasmdoasdlöase')
   },
   computed: {
     people () {
@@ -60,9 +30,6 @@ export default {
     },
     filterRunningOut () {
       return this.$store.state.user.filterRunningOut
-    },
-    baseURL () {
-      return 'https://pxe6lsvbkl.execute-api.eu-north-1.amazonaws.com/default'
     }
   },
   components: {
--- OldMedicine/src/store/user/mutations.js
+++ OldMedicine/src/store/user/mutations.js
@@ -1,3 +1,6 @@
+import axios from 'axios'
+const baseURL = 'https://pxe6lsvbkl.execute-api.eu-north-1.amazonaws.com/default/'
+
 export const updateFilterForgotten = (state, filterForgotten) => {
   state.filterForgotten = filterForgotten
 }
@@ -6,7 +9,28 @@ export const updateFilterRunningOut = (state, filterRunningOut) => {
   state.filterRunningOut = filterRunningOut
 }
 
-export const addPerson = (state, nameAndId) => {
+export const getInit = (state, clientId) => {
+  var queryParameters = '?client_id=' + clientId
+  axios.get(baseURL + 'connections/get' + queryParameters).then(response1 => {
+    for (let i = 0; i < response1.data.body.length; i++) {
+      console.log(response1.data.body[i])
+      axios.get(baseURL + 'information-patient/get' + '?patient_id=' + response1.data.body[i].patient_id).then(response2 => {
+        console.log(response2.data.body)
+      }).catch(function (error) {
+        console.log(error)
+      })
+      axios.get(baseURL + 'medicine/get' + '?patient_id=' + response1.data.body[i].patient_id).then(response2 => {
+        console.log(response2.data.body)
+      }).catch(function (error) {
+        console.log(error)
+      })
+    }
+  }).catch(function (error) {
+    console.log(error)
+  })
+}
+
+export const addPerson = (state, nameAndId) => { // denna person ska läggas till i state men även skickas till api
   var element = {
     index: state.people.length,
     name: nameAndId.name,
@@ -16,7 +40,7 @@ export const addPerson = (state, nameAndId) => {
   state.people.push(element)
 }
 
-export const addMedicine = (state, medicineAndState) => {
+export const addMedicine = (state, medicineAndState) => { // denna medicin ska läggas till i store men även skickas till api (använd person id för att veta hoss vem medicinen ska läggas till)
   var medicine = {
     personPointer: medicineAndState.personPointer,
     index: state.people[medicineAndState.personPointer].medications.length,
@@ -35,3 +59,47 @@ export const changePersonPointer = (state, personPointer) => {
 export const changeMedicinePointer = (state, medicinePointer) => {
   state.medicinePointer = medicinePointer
 }
+
+// export const getStore = (state, body) => {
+//   var queryParameters = '?'
+//   for (const key in body) {
+//     queryParameters += key + '=' + body[key] + '&'
+//   }
+//   queryParameters = queryParameters.slice(0, -1)
+
+//   axios.get(baseURL + 'connections/get' + queryParameters).then(response => {
+//     console.log(response)
+//     // state.people.push(response.data.body)
+//   }).catch(function (error) {
+//     console.log(error)
+//   })
+// }
+// request (endpoint, body, method) { // Exempel: ('connections/get', {'client_id': CLIENT_ID_HERE}, 'get'}
+//   if (method === 'get') {
+//     var queryParameters = '?'
+//     for (const key in body) {
+//       queryParameters += key + '=' + body[key] + '&'
+//     }
+//     queryParameters = queryParameters.slice(0, -1)
+
+//     console.log(this.baseURL + endpoint + queryParameters)
+//     axios.get(this.baseURL + endpoint + queryParameters).then(response => {
+//       console.log(response.data.body)
+//       return response.data.body
+//     }).catch(function (error) {
+//       console.log(error)
+//     })
+//   } else {
+//     axios({
+//       method: method,
+//       url: this.baseURL + endpoint,
+//       headers: {},
+//       data: body
+//     }).then(response => {
+//       console.log(response)
+//       return response.data.body
+//     }).catch(function (error) {
+//       console.log(error)
+//     })
+//   }
+// }
